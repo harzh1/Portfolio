5da555e62b871421d36cb1de8da4e3805ac703e3
Harsh Raj <101712553+harzh1@users.noreply.github.com>
2025-06-15 23:22:57 +0530

    gh-pages for deployment added

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// IMPORTANT: Add the correct repo name to `base`
 export default defineConfig({
+  base: "/Portfolio/", // <-- repo name here
   plugins: [react(), tailwindcss()],
 });
